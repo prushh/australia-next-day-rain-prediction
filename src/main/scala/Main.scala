288468337dcdff1a7f699aa975be0be1adbdca84
prushh <davide.pruscini@gmail.com>
2022-11-09 19:50:01 +0100

    Tested SVMWithSGD model by spark.mllib
    source: https://spark.apache.org/docs/latest/mllib-linear-methods.html#linear-support-vector-machines-svms

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,14 +19,24 @@ import scala.util.Using
     val sparkSession = SparkProjectConfig.sparkSession("local[*]", 1)
     val sparkContext = sparkSession.sparkContext
 
-    val url = ClassLoader.getSystemResource("weatherAUS.json")
-    val schemaSource = Source.fromFile(url.getFile)
-    val schemaFromJson = DataType.fromJson(schemaSource.getLines().mkString).asInstanceOf[StructType]
-
-    val df = sparkSession.read
-      .option("header", value = true)
-      .option("timestampFormatter", TimestampFormatter.timestampPattern)
-      .schema(schemaFromJson)
-      .csv("data/weatherAUS.csv")
+    val data = MLUtils.loadLibSVMFile(sparkContext, "data/sample_libsvm_data.txt")
+    // Split data into training (60%) and test (40%).
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val training = splits(0).cache()
+    val test = splits(1)
+    // Run training algorithm to build the model
+    val numIterations = 100
+    val model = SVMWithSGD.train(training, numIterations)
+    // Clear the default threshold.
+    model.clearThreshold()
+    // Compute raw scores on the test set.
+    val scoreAndLabels = test.map { point =>
+        val score = model.predict(point.features)
+        (score, point.label)
+    }
+    // Get evaluation metrics.
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+    val auROC = metrics.areaUnderROC()
+    println(s"Area under ROC = $auROC")
 
     sparkSession.stop()
