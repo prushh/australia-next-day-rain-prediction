a84d0c2f834e65db0a499706a3dd3c542d0704ae
Chiara Manca <48201839+chiarasharp@users.noreply.github.com>
2022-11-22 18:48:47 +0100

    created libsvm format file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,21 +19,26 @@ import scala.util.Using
     val sparkSession = SparkProjectConfig.sparkSession("local[*]", 1)
     val sparkContext = sparkSession.sparkContext
 
-    val data = MLUtils.loadLibSVMFile(sparkContext, "data/sample_libsvm_data.txt")
+    val data = MLUtils.loadLibSVMFile(sparkContext, "data/weatherAUSlibsvm.txt")
+
     // Split data into training (60%) and test (40%).
     val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
     val training = splits(0).cache()
     val test = splits(1)
+
     // Run training algorithm to build the model
     val numIterations = 100
     val model = SVMWithSGD.train(training, numIterations)
+
     // Clear the default threshold.
     model.clearThreshold()
+
     // Compute raw scores on the test set.
     val scoreAndLabels = test.map { point =>
         val score = model.predict(point.features)
         (score, point.label)
     }
+
     // Get evaluation metrics.
     val metrics = new BinaryClassificationMetrics(scoreAndLabels)
     val auROC = metrics.areaUnderROC()
