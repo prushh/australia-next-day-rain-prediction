cf81125054806c75abc8e571e4194a33bb3d3b61
cotus997 <giosue.cotugno@studio.unibo.it>
2023-03-13 23:41:00 +0100

    working map-reduce on knn
    TODO: work on accuracy function and make it compatible with our dataset

--- src/main/scala/algorithm/knn/kNN.scala
+++ src/main/scala/algorithm/knn/kNN.scala
@@ -1,61 +1,90 @@
 package it.unibo.andrp
 package algorithm.knn
 
+
 import model.DataPoint
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
+import scala.collection.mutable.ListBuffer
+import scala.math.Ordering.Implicits._
+
 object KNN {
-    def knn(data: RDD[DataPoint], testPoint: Seq[Double], k: Int): Double = {
-        val distances = data.map{ point =>
-            val dist = euclideanDistance(point.features, testPoint)
-            (point.label, dist)
-        }
 
-        val kNearestNeighbors = distances.takeOrdered(k)(Ordering[Double].on(x => x._2))
 
-        val labels = kNearestNeighbors.map(_._1)
-
-        majorityVote(labels)
+  def knnMapReduce(data: RDD[DataPoint], k: Int, query: DataPoint): Double = {
+    // Map phase: calculate distances between the query point and all data points
+    val distances = data.map { x =>
+      (x, euclideanDistance(query, x))
     }
 
-    def euclideanDistance(features1: Seq[Double], features2: Seq[Double]): Double = {
-        math.sqrt(features1.zip(features2).map{ case(x, y) => math.pow(x-y, 2) }.sum)
+    // Reduce phase: collect the k nearest neighbors
+    val nearestNeighbors = distances
+      .takeOrdered(k)(Ordering.by { case (_, distance) => distance })
+      .groupBy(_._1)
+      .mapValues(_.size)
+
+    // restituisco la classe con la maggioranza dei voti
+    nearestNeighbors.maxBy { case (_, count) => count }._1.label
+  }
+
+
+  def knn(data: RDD[DataPoint], testPoint: Seq[Double], k: Int): Double = {
+    val distances = data.map { point =>
+      val dist = euclideanDistance(point, point)
+      (point.label, dist)
     }
 
-    def majorityVote(labels: Array[Double]): Double = {
-        val labelCounts = labels.groupBy(identity).mapValues(_.length)
-        labelCounts.maxBy(_._2)._1
-    }
+    val kNearestNeighbors = distances.takeOrdered(k)(Ordering[Double].on(x => x._2))
 
-    def accuracy(data: RDD[DataPoint], testData: RDD[DataPoint], k: Int): Double = {
-        val predictions = testData.map(testPoint => knn(data, testPoint.features, k))
-        val actualLabels = testData.map(_.label)
-        val correctPredictions = predictions.zip(actualLabels).filter{ case(prediction, actualLabel) =>
-            prediction == actualLabel
-        }.count()
-        val totalPredictions = testData.count()
-        correctPredictions.toDouble / totalPredictions.toDouble
+    val labels = kNearestNeighbors.map(_._1)
+
+    majorityVote(labels)
+  }
+
+  def euclideanDistance(features1: DataPoint, features2: DataPoint): Double = {
+    math.sqrt(features1.features.zip(features2.features).map { case (x, y) => math.pow(x - y, 2) }.sum)
+  }
+
+  def majorityVote(labels: Array[Double]): Double = {
+    val labelCounts = labels.groupBy(identity).mapValues(_.length)
+    labelCounts.maxBy(_._2)._1
+  }
+
+  def accuracy(data: RDD[DataPoint], testData: Seq[DataPoint], k: Int): Double = {
+    val predictions = testData.map(testPoint => knnMapReduce(data, k, testPoint))
+    val actualLabels = testData.map(_.label)
+    val correctPredictions = predictions.zip(actualLabels).count {
+      case (prediction, actualLabel) => {
+        prediction == actualLabel
+      }
     }
+    val totalPredictions = testData.length
+    correctPredictions.toDouble / totalPredictions.toDouble
+  }
 }
 
 object Main {
-    def main(args: Array[String]): Unit = {
-        val sc = new SparkContext("local[*]", "KNN")
-        val data = sc.parallelize(Seq(
-            DataPoint(Seq(1.0, 2.0), 0.0),
-            DataPoint(Seq(2.0, 3.0), 1.0),
-            DataPoint(Seq(3.0, 4.0), 1.0),
-            DataPoint(Seq(4.0, 5.0), 0.0)
-        ))
-        val testData = sc.parallelize(Seq(
-            DataPoint(Seq(5.0, 6.0), 0.0),
-            DataPoint(Seq(6.0, 7.0), 1.0)
-        ))
-        val k = 3
-        val accuracy = KNN.accuracy(data, testData, k)
-        println(s"Accuracy: $accuracy")
-    }
+  def main(args: Array[String]): Unit = {
+    import org.apache.spark.{SparkConf, SparkContext}
+
+    val conf = new SparkConf().setAppName("myApp").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val data = sc.parallelize(Seq(
+      DataPoint(Seq(1.0, 2.0), 0.0),
+      DataPoint(Seq(2.0, 3.0), 1.0),
+      DataPoint(Seq(3.0, 4.0), 1.0),
+      DataPoint(Seq(4.0, 5.0), 0.0)
+    ))
+    val testData = Seq(
+      DataPoint(Seq(5.0, 6.0), 0.0),
+      DataPoint(Seq(6.0, 7.0), 1.0)
+    )
+
+    val k = 3
+    val accuracy = KNN.accuracy(data, testData, k)
+    println(s"Accuracy: $accuracy")
+  }
 }
 
 
