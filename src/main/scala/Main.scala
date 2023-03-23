56f04f206595ead9a639cfc170558f0a0be1356f
cotus997 <giosue.cotugno@studio.unibo.it>
2023-03-23 18:33:16 +0100

    added knn par and not

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,43 +11,75 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 @main def Main(classifier: String, master: String, dataset: String, parallelism: Int, limit: Boolean, simulation: Boolean): Unit = {
-    /*
-     * Checking arguments.
-     */
-    val datasetPath = s"data/$dataset"
-    val numRows = if (limit) 1000 else 60000 // cardinality: 63754
+  /*
+   * Checking arguments.
+   */
+  val datasetPath = s"data/$dataset"
+  val numRows = if (limit) 1000 else 60000 // cardinality: 63754
 
-    if (!simulation) {
-        println("Configuration:")
-        println(s"- algorithm: $classifier")
-        println(s"- master: $master")
-        println(s"- dataset path: $datasetPath")
-        println(s"- initialized Spark Context with parallelism: $parallelism")
-    }
+  if (!simulation) {
+    println("Configuration:")
+    println(s"- algorithm: $classifier")
+    println(s"- master: $master")
+    println(s"- dataset path: $datasetPath")
+    println(s"- initialized Spark Context with parallelism: $parallelism")
+  }
 
-    /*
-     * Loading Spark and Hadoop.
-     */
-    val sparkSession = SparkProjectConfig.sparkSession(master, parallelism)
-    val sparkContext = sparkSession.sparkContext
+  /*
+   * Loading Spark and Hadoop.
+   */
+  val sparkSession = SparkProjectConfig.sparkSession(master, parallelism)
+  val sparkContext = sparkSession.sparkContext
 
-    /*
-     * Loading the dataset.
-     */
-    val csvDataset = sparkSession.read
-      .option("header", value = true)
-      .csv(datasetPath)
-      .limit(numRows)
+  /*
+   * Loading the dataset.
+   */
+  val csvDataset = sparkSession.read
+    .option("header", value = true)
+    .csv(datasetPath)
+    .limit(numRows)
 
-    println("Dataset loaded!")
+  println("Dataset loaded!")
 
-    val (trainingData, testData) = getDataPoints(csvDataset)
-    val testDataArray = testData.collect()
+  val (trainingData, testData) = getDataPoints(csvDataset)
+  val testDataArray = testData.collect()
 
-    if (simulation) {
+  if (simulation) {
+    val knnClassifier = algorithm.knn.classify(sparkContext) _
+    time("[knn]", knnClassifier(trainingData, testDataArray, true))
+
+    val knnClassifierseq = algorithm.knn.classify(sparkContext) _
+    time("[knn seq]", knnClassifierseq(trainingData, testDataArray, false))
+
+    val decisionTree = DecisionTreeOld(par = true)
+    time("[decision-tree]", decisionTree.train(trainingData))
+    println(s"[decision-tree] - accuracy: ${decisionTree.accuracy(testDataArray.toList)}")
+
+    val decisionTreeSeq = DecisionTreeOld(par = false)
+    time("[decision-tree]", decisionTreeSeq.train(trainingData))
+    println(s"[decision-tree seq] - accuracy: ${decisionTreeSeq.accuracy(testDataArray.toList)}")
+
+    val decisionTreeSpark = DecisionTreeS()
+    time("[decision-tree] - spark", decisionTreeSpark.fit(trainingData))
+    println(s"[decision-tree] - spark - accuracy: ${decisionTreeSpark.accuracy(testData)}")
+
+    val randomForest = RandomForest(par = true)
+    time("[random-forest]", randomForest.train(trainingData))
+    println(s"[random-forest] - accuracy: ${randomForest.accuracy(testDataArray.toList)}")
+
+    val randomForestSeq = RandomForest(par = false)
+    time("[random-forest Seq]", randomForestSeq.train(trainingData))
+    println(s"[random-forest Seq] - accuracy: ${randomForestSeq.accuracy(testDataArray.toList)}")
+
+    val randomForestSpark = RandomForestS()
+    time("[random-forest] - spark", randomForestSpark.fit(trainingData))
+    println(s"[random-forest] - spark - accuracy: ${randomForestSpark.accuracy(testData)}")
+  } else {
+    classifier match {
+      case "knn" =>
         val knnClassifier = algorithm.knn.classify(sparkContext) _
-        time("[knn]", knnClassifier(trainingData, testDataArray))
-
+        time("[kNN]", knnClassifier(trainingData, testDataArray,true))
+      case "decision-tree" =>
         val decisionTree = DecisionTreeOld(par = false)
         time("[decision-tree]", decisionTree.train(trainingData))
         println(s"[decision-tree] - accuracy: ${decisionTree.accuracy(testDataArray.toList)}")
@@ -55,35 +87,14 @@ import org.apache.spark.sql.SparkSession
         val decisionTreeSpark = DecisionTreeS()
         time("[decision-tree] - spark", decisionTreeSpark.fit(trainingData))
         println(s"[decision-tree] - spark - accuracy: ${decisionTreeSpark.accuracy(testData)}")
-
+      case "random-forest" =>
         val randomForest = RandomForest(par = false)
         time("[random-forest]", randomForest.train(trainingData))
         println(s"[random-forest] - accuracy: ${randomForest.accuracy(testDataArray.toList)}")
 
-        // val randomForestSpark = RandomForestS()
-        //        time("[random-forest] - spark", randomForestSpark.fit(trainingData))
-        //        println(s"[random-forest] - spark - accuracy: ${randomForestSpark.accuracy(testData)}")
-    } else {
-        classifier match {
-            case "knn" =>
-                val knnClassifier = algorithm.knn.classify(sparkContext) _
-                time("[kNN]", knnClassifier(trainingData, testDataArray))
-            case "decision-tree" =>
-                val decisionTree = DecisionTreeOld(par = false)
-                time("[decision-tree]", decisionTree.train(trainingData))
-                println(s"[decision-tree] - accuracy: ${decisionTree.accuracy(testDataArray.toList)}")
-
-                val decisionTreeSpark = DecisionTreeS()
-                time("[decision-tree] - spark", decisionTreeSpark.fit(trainingData))
-                println(s"[decision-tree] - spark - accuracy: ${decisionTreeSpark.accuracy(testData)}")
-            case "random-forest" =>
-                val randomForest = RandomForest(par = false)
-                time("[random-forest]", randomForest.train(trainingData))
-                println(s"[random-forest] - accuracy: ${randomForest.accuracy(testDataArray.toList)}")
-
-                val randomForestSpark = RandomForestS()
-                time("[random-forest] - spark", randomForestSpark.fit(trainingData))
-                println(s"[random-forest] - spark - accuracy: ${randomForestSpark.accuracy(testData)}")
-        }
+        val randomForestSpark = RandomForestS()
+        time("[random-forest] - spark", randomForestSpark.fit(trainingData))
+        println(s"[random-forest] - spark - accuracy: ${randomForestSpark.accuracy(testData)}")
     }
+  }
 }
--- src/main/scala/algorithm/knn/package.scala
+++ src/main/scala/algorithm/knn/package.scala
@@ -10,53 +10,83 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Row}
 
 package object knn {
-    def classify(sparkContext: SparkContext)(trainData: RDD[DataPoint], testData: Seq[DataPoint]): Unit = {
-        // Choose distance function
-        val distance: DistanceFunc = AlgorithmConfig.Knn.DISTANCE_METHOD match {
-            case Distances.Euclidean => Distances.euclidean
-            case Distances.Manhattan => Distances.manhattan
-        }
-
-        // Compute predictions
-        val predictions = testData.map {
-            testPoint =>
-                _classify(
-                    trainData,
-                    testPoint,
-                    AlgorithmConfig.Knn.NUMBER_NEAREST_NEIGHBORS,
-                    distance
-                )
-        }
-
-        // Get only labels
-        val labels = testData map {
-            _.label
-        }
-
-        // Count correct predictions by comparing them with the labels
-        val correctPredictions = (predictions zip labels).count {
-            case (prediction, label) => prediction == label
-        }
-
-        val totalPredictions = testData.length
-        val accuracy = correctPredictions.toDouble / totalPredictions.toDouble
-        println(s"[knn] - accuracy: $accuracy")
+  def classify(sparkContext: SparkContext)(trainData: RDD[DataPoint], testData: Seq[DataPoint], par: Boolean = true): Unit = {
+    // Choose distance function
+    val distance: DistanceFunc = AlgorithmConfig.Knn.DISTANCE_METHOD match {
+      case Distances.Euclidean => Distances.euclidean
+      case Distances.Manhattan => Distances.manhattan
+    }
+    var predictions: Seq[Double] = Seq()
+    if (par) {
+      // Compute predictions
+      predictions = testData.map {
+        testPoint =>
+          _classify(
+            trainData,
+            testPoint,
+            AlgorithmConfig.Knn.NUMBER_NEAREST_NEIGHBORS,
+            distance
+          )
+      }
+    } else {
+      predictions = testData.map {
+        testPoint =>
+          _classifySeq(
+            trainData.collect(),
+            testPoint,
+            AlgorithmConfig.Knn.NUMBER_NEAREST_NEIGHBORS,
+            distance
+          )
+      }
     }
 
-    private def _classify(trainData: RDD[DataPoint], testPoint: DataPoint, k: Int, distance: DistanceFunc): Double = {
-        // Compute distances
-        val distances = trainData map { point =>
-            val dist = distance(testPoint, point)
-            (point.label, dist)
-        }
 
-        // Get kNNs based on distances value
-        val kNearestNeighbors = (distances takeOrdered k)(Ordering[Double] on (_._2))
-        // Get only labels
-        val labels = kNearestNeighbors map (_._1)
-        // Count occurrences for each label value
-        val labelCounts = (labels groupBy identity).view.mapValues(_.length)
-        // Get label with the highest count
-        labelCounts.maxBy(_._2)._1
+    // Get only labels
+    val labels = testData map {
+      _.label
     }
+
+    // Count correct predictions by comparing them with the labels
+    val correctPredictions = (predictions zip labels).count {
+      case (prediction, label) => prediction == label
+    }
+
+    val totalPredictions = testData.length
+    val accuracy = correctPredictions.toDouble / totalPredictions.toDouble
+    println(s"[knn] - accuracy: $accuracy")
+  }
+
+  private def _classify(trainData: RDD[DataPoint], testPoint: DataPoint, k: Int, distance: DistanceFunc): Double = {
+    // Compute distances
+    val distances = trainData map { point =>
+      val dist = distance(testPoint, point)
+      (point.label, dist)
+    }
+
+    // Get kNNs based on distances value
+    val kNearestNeighbors = (distances takeOrdered k) (Ordering[Double] on (_._2))
+    // Get only labels
+    val labels = kNearestNeighbors map (_._1)
+    // Count occurrences for each label value
+    val labelCounts = (labels groupBy identity).view.mapValues(_.length)
+    // Get label with the highest count
+    labelCounts.maxBy(_._2)._1
+  }
+
+  private def _classifySeq(trainData: Seq[DataPoint], testPoint: DataPoint, k: Int, distance: DistanceFunc): Double = {
+    // Compute distances
+    val distances = trainData map { point =>
+      val dist = distance(testPoint, point)
+      (point.label, dist)
+    }
+
+    // Get kNNs based on distances value
+    val kNearestNeighbors = distances.sortBy(_._2).take(k)
+    // Get only labels
+    val labels = kNearestNeighbors map (_._1)
+    // Count occurrences for each label value
+    val labelCounts = (labels groupBy identity).view.mapValues(_.length)
+    // Get label with the highest count
+    labelCounts.maxBy(_._2)._1
+  }
 }
